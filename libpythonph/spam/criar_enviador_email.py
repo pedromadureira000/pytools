fc881197bc6270be91ab79c8957a96ce7be1a3b5
phsw <pedromadureira000@gmail.com>
2021-02-09 09:31:16 -0300

    testando lançamento de exceções com pytest usando gerenciador de contexto.

---
+++ libpythonph/spam/criar_enviador_email.py
@@ -0,0 +1,9 @@
+class Enviador:
+    def enviar(self, destinatario, remetente, assunto, corpo):
+        if '@' not in destinatario:
+            raise EmailInvalido(f"Email de destinatario invalido {destinatario}")
+        return destinatario
+
+
+class EmailInvalido(Exception):
+    pass
--- libpythonph/tests/test_spam/test_criar_enviador_email.py
+++
@@ -1,3 +0,0 @@
-def test_criar_enviador_email():
-    enviador = Enviador()
-    assert enviador is not None
